5c1832b2373a5279af44717308aac045f1ce5af6
Oluro Olaoluwa <ollaollu@gmail.com>
2017-03-23 05:48:02 +0100

    Initial Commit

---
+++ Groupa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Groupa
+//
+//  Created by ollaollu on 23/03/2017.
+//  Copyright © 2017 ollaollu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
